5622a370a4e2eede46d8dc9b30ce57e2203ac8a7
Charlie_jin <64459326+Ashkar2001@users.noreply.github.com>
2020-05-25 22:36:50 +0530

    Update chat_status.py

--- tg_bot/modules/helper_funcs/chat_status.py
+++ tg_bot/modules/helper_funcs/chat_status.py
@@ -124,7 +124,7 @@ def whitelist_plus(func):
         if user and is_whitelist_plus(chat, user.id):
             return func(bot, update, *args, **kwargs)
         else:
-            update.effective_message.reply_text("You don't have access to use this.\nVisit @Charlie_jin")
+            update.effective_message.reply_text("You don't have access to use this.\nVisit @Charlie_jin ")
 
     return is_whitelist_plus_func
 
