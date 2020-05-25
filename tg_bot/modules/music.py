62890ffd7650aa76c3ec45db26420b85e7e9783a
Charlie_jin <64459326+Ashkar2001@users.noreply.github.com>
2020-05-25 17:19:13 +0530

    Update music.py

--- tg_bot/modules/music.py
+++ tg_bot/modules/music.py
@@ -22,12 +22,12 @@ def music(bot: Bot, update: Update, args):
 	if video_id.find('youtu.be') != -1:
 		index = video_id.rfind('/') + 1
 		video_id = video_id[index:][:11]
-		message.reply_text("Please wait...\ndownloading audio.")
+		message.reply_text("Please wait...\ndownloading audio🤗.")
 
 	elif video_id.find('youtube') != -1:
 		index = video_id.rfind('?v=') + 3
 		video_id = video_id[index:][:11]
-		message.reply_text("Please wait...\downloading audio.")
+		message.reply_text("Please wait...\downloading audio🤗.")
 
 	elif not video_id.find('youtube') != -1:
 		message.reply_text("Please provide me youtube link")
